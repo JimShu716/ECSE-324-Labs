78ef73aa15e37fc401bb003fa7aedd89550c0ebc
OldPineapple <46538736+OldPineapple@users.noreply.github.com>
2019-10-10 15:20:22 -0400

    Add files via upload

--- G55_Lab2/getting_started.c
+++ G55_Lab2/getting_started.c
@@ -1,12 +1,15 @@
+#include <stdio.h>
 int main(){
 			
 		int a[5] = {1,20,3,4,5};
 		int min_val = a[0];			//initialize the minimum value
 		int i;					//pointer to search for the minimum value
 		for(i=0;i<5;i++){
-			if a[i]< min_val{
+			if (a[i]< min_val){
 				min_val = a[i];		//update the minimum value
 			}
 		}
-	return min_val;
+	printf("The minimum value is %d\n", min_val);
+	return min_val;	
+
 }
