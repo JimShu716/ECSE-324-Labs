43e884ff662fa6ef05da076d70a4c6b8a361eff3
Hao Shu <shuhao716@outlook.com>
2019-10-10 14:35:19 -0400

    Update getting_started.c

--- G55_Lab2/getting_started.c
+++ G55_Lab2/getting_started.c
@@ -1,14 +1,12 @@
 int main(){
 			
 		int a[5] = {1,20,3,4,5};
-		int min_val = a[0];
-		int i;
+		int min_val = a[0];			//initialize the minimum value
+		int i;					//pointer to search for the minimum value
 		for(i=0;i<5;i++){
-					
-					if a[i]< min_val{
-							min_val = a[i];
-				}
+			if a[i]< min_val{
+				min_val = a[i];		//update the minimum value
 			}
-		
-		return min_val;
+		}
+	return min_val;
 }
