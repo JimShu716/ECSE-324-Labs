3572da68805f69d57ab5c45c2a4f99f9feb516f7
OldPineapple <46538736+OldPineapple@users.noreply.github.com>
2019-10-30 23:21:30 -0400

    Add files via upload

--- G55_lab2/main_HEX.c
+++ G55_lab2/main_HEX.c
@@ -6,26 +6,27 @@
 
 int main() {
 	while(1){
-		write_LEDs_ASM(read_slider_switches_ASM());
+		int sw = read_slider_switches_ASM();
+		write_LEDs_ASM(sw);
+		if (sw == 512) {
+			HEX_clear_ASM(HEX0|HEX1|HEX2|HEX3|HEX4|HEX5);
+		} else {
 		HEX_flood_ASM(HEX4|HEX5);
 		HEX_clear_ASM(HEX0|HEX1|HEX2|HEX3);
-		HEX_write_ASM(HEX0, 6);
 		switch(PB_data_is_pressed_ASM(read_PB_data_ASM())){		    // get button number that is pressed
 			case 0:
-				HEX_write_ASM(HEX0, read_slider_switches_ASM());	// display on HEX 0
+				HEX_write_ASM(HEX0, sw);	// display on HEX 0
 				break;
 			case 1:
-				HEX_write_ASM(HEX1, read_slider_switches_ASM());	// display on HEX 1
+				HEX_write_ASM(HEX1, sw);	// display on HEX 1
 				break;
 			case 2:
-				HEX_write_ASM(HEX2, read_slider_switches_ASM());	// display on HEX 2
+				HEX_write_ASM(HEX2, sw);	// display on HEX 2
 				break;
 			case 3:
-				HEX_write_ASM(HEX3, read_slider_switches_ASM());	// display on HEX 3
+				HEX_write_ASM(HEX3, sw);	// display on HEX 3
 				break;	
-		}
-		if (read_slider_switches_ASM() >= 512){
-			HEX_clear_ASM(HEX0|HEX1|HEX2|HEX3|HEX4|HEX5);
+			}
 		}
 	}
 
