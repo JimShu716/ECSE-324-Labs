451fd1bb4bbe6e265ae8eb7644b13d2586ec3968
OldPineapple <46538736+OldPineapple@users.noreply.github.com>
2019-11-09 21:14:12 -0500

    Update main_HEX.c

--- G55_lab2/main_HEX.c
+++ G55_lab2/main_HEX.c
@@ -6,28 +6,28 @@
 
 int main() {
 	while(1){
-		int sw = read_slider_switches_ASM();
-		write_LEDs_ASM(sw);
-		if (sw >= 512) {
+		int SW = read_slider_switches_ASM();
+		write_LEDs_ASM(SW);
+		if (SW >= 512) {
 			HEX_clear_ASM(HEX0|HEX1|HEX2|HEX3|HEX4|HEX5);
 		} else {
 		HEX_flood_ASM(HEX4|HEX5);
-		switch(PB_data_is_pressed_ASM(read_PB_data_ASM())){		    // get button number that is pressed
+		switch(PB_data_is_pressed_ASM(read_PB_data_ASM())){
 			case 0:
 				HEX_clear_ASM(HEX0|HEX1|HEX2|HEX3);
-				HEX_write_ASM(HEX0, sw);	// display on HEX 0
+				HEX_write_ASM(HEX0, SW);	// write value on HEX0
 				break;
 			case 1:
 				HEX_clear_ASM(HEX0|HEX1|HEX2|HEX3);
-				HEX_write_ASM(HEX1, sw);	// display on HEX 1
+				HEX_write_ASM(HEX1, SW);	// write value on HEX1
 				break;
 			case 2:
 				HEX_clear_ASM(HEX0|HEX1|HEX2|HEX3);
-				HEX_write_ASM(HEX2, sw);	// display on HEX 2
+				HEX_write_ASM(HEX2, SW);	// write value on HEX2
 				break;
 			case 3:
 				HEX_clear_ASM(HEX0|HEX1|HEX2|HEX3);
-				HEX_write_ASM(HEX3, sw);	// display on HEX 3
+				HEX_write_ASM(HEX3, SW);	// write value on HEX3
 				break;	
 			}
 		}
