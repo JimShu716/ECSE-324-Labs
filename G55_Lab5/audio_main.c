cdeaeb6a3e66dc7e66ccc5a71439294ee738f827
OldPineapple <46538736+OldPineapple@users.noreply.github.com>
2019-11-22 15:25:44 -0500

    Add files via upload

--- G55_Lab5/audio_main.c
+++ G55_Lab5/audio_main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 #include "./drivers/inc/audio1.h"
 
@@ -6,16 +7,20 @@ int main() {
 	int frequency = 100;
 	int sample_rate = 48000;
 	int half_cycle = sample_rate / (frequency * 2);
-	while(1) {
-		int i;
-		for (i = 0; i< half_cycle; ){
-              if(write_audio_ASM(0x00FFFFFF)){
-                   i++;}
+	int x = 0;
+	int y = 0;
+
+	while(1){
+		while (x < half_cycle){
+        	if(write_audio_ASM(0x00FFFFFF))
+                   x++;
          }
-			for (i = 0; i< half_cycle; ){
-              if(write_audio_ASM(0x00000000)){
-                   i++;}
-         }		
+		while (y < half_cycle){
+        	if(write_audio_ASM(0x00000000))
+                   y++;
+         }
+		x = 0;
+		y = 0;
 	}
 
 	return 0;
